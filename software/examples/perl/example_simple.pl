9eeb66cf02bd33e116c41f9991e0cacd530861c5
Matthias Bolte <matthias@tinkerforge.com>
2015-08-25 16:26:42 +0200

    Fix typo

--- software/examples/perl/example_simple.pl
+++ software/examples/perl/example_simple.pl
@@ -14,7 +14,7 @@ $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
 # Get current acceleration (unit is g/1000)
-my ($x, $y, $z) = $acc->get_acceleration();
+my ($x, $y, $z) = $a->get_acceleration();
 
 print "Acceleration(X): " . $x/1000.0 . " g\n";
 print "Acceleration(Y): " . $y/1000.0 . " g\n";
