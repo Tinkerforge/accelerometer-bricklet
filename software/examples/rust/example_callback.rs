e92564823b7faddc1935ba43841a62682843b302
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 18:23:04 +0100

    rust: Rename callback receivers.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -12,10 +12,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
                                           // Don't use device before ipcon is connected.
 
-    // Create receiver for acceleration events.
-    let acceleration_receiver = a.get_acceleration_receiver();
+    let acceleration_receiver = a.get_acceleration_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `a` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `a` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for acceleration in acceleration_receiver {
